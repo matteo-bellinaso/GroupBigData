313ab4b9a7d4fb7f1645ca715052fbc1051c60ba
matteo-bellinaso <matteo.bellinaso@gmail.com>
2019-03-05 18:01:14 +0100

    write to csv

--- src/main/java/utility/PropertyEnum.java
+++ src/main/java/utility/PropertyEnum.java
@@ -1,17 +1,18 @@
 package utility;
 
 public class PropertyEnum {
-    public static final String  user="user";
-    public static final String  password="password";
-    public static final String  dbUrl="databaseUrl";
-    public static final String  driver="driver";
-    public static final String  downloadLocation="downloadLocation";
-    public static final String  jsonLocation="jsonLocation";
+    public static final String user = "user";
+    public static final String password = "password";
+    public static final String dbUrl = "databaseUrl";
+    public static final String driver = "driver";
+    public static final String downloadLocation = "downloadLocation";
+    public static final String jsonLocation = "jsonLocation";
+    public static final String csvLocation = "csvLocation";
 
     //spark alias
-    public static final String  sparkMaster="spark.master";
-    public static final String  sparkAppName="spark.app.name";
-    public static final String  sparkDriverMemory="spark.driver.memory";
+    public static final String sparkMaster = "spark.master";
+    public static final String sparkAppName = "spark.app.name";
+    public static final String sparkDriverMemory = "spark.driver.memory";
 
 
 }
--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,9 +1,14 @@
+import entity.Actor
 import fileUtilities.{FileDownloader, FileExtractor}
 import org.apache.commons.configuration.ConfigurationFactory
+import org.apache.spark.SparkContext
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.DataFrame
 import proprierties.Converter
 import persistence.ConnectionProvider
 import properties.{ApplicationConfig, SparkConfig}
 import utility.{Paths, PropertyEnum}
+
 
 object main {
   def main(args: Array[String]): Unit = {
@@ -14,7 +19,8 @@ object main {
 
     val contex = SparkConfig.instance().setSparkConfiguration()
 
-    val config  = SparkConfig.instance()
+
+    val config = SparkConfig.instance()
 
     val filename = new FileDownloader().downloadWithRedirect("http://data.githubarchive.org/2018-03-01-0.json.gz")
     val fileExtractor = new FileExtractor
@@ -22,15 +28,19 @@ object main {
       ApplicationConfig.instance().getProperty(PropertyEnum.downloadLocation) + filename,
       ApplicationConfig.instance().getProperty(PropertyEnum.jsonLocation) + cutExtensionFromFilename(filename) + "-" + System.currentTimeMillis() + ".json")
 
-    val strunzDF = Converter.ConvertJSONToDS(path , contex)
+    val strunzDF = Converter.ConvertJSONToDS(path, contex)
 
     val strunzRDD = Converter.ConvertJSONToRDD(path, contex)
 
+    val actorDF = strunzDF.select("actor.*").dropDuplicates("id")
+
+    actorDF.write.csv(PropertyEnum.csvLocation + "actor")
   }
 
   def cutExtensionFromFilename(filename: String): String = {
     val splittedFileName = filename.split("\\.")
     splittedFileName(0)
   }
+
 
 }
