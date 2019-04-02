c589da170cd6189dbc2edc4b9191db3666026698
matteo-bellinaso <matteo.bellinaso@gmail.com>
2019-04-02 12:27:59 +0200

    save csv DataFrame

--- src/main/scala/converter/SaveCsvDataFrame.scala
+++ src/main/scala/converter/SaveCsvDataFrame.scala
@@ -1,20 +1,27 @@
 package converter
 
-import java.io.File
+import org.apache.hadoop.fs.{FileSystem, Path}
+import org.apache.spark.SparkContext
+import org.apache.spark.sql.{DataFrame}
 
-import org.apache.spark.sql.DataFrame
+class SaveCsvDataFrame(path: String, sc: SparkContext) {
 
-class SaveCsvDataFrame(path: String) {
+  val fs = FileSystem.get(sc.hadoopConfiguration)
 
   def save(dataFrame: DataFrame, fileName: String) = {
 
-    val file = new File(path + fileName)
+    val outPath = path + "/" + fileName
 
-    if (file == null) {
+    val pathFinal = new Path(outPath)
+
+    if (!fs.exists(pathFinal)) {
       dataFrame.repartition(1).write.csv(path + "/" + fileName)
-
+    } else {
+      if (fs.delete(pathFinal, true)) {
+        dataFrame.repartition(1).write.csv(path + "/" + fileName)
+      } else {
+        println("problema con l'eleminazione del file")
+      }
     }
-
   }
-
 }
