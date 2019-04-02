9af774172ba391c44ca8b1a2f5542af0fec8d763
matteo-bellinaso <matteo.bellinaso@gmail.com>
2019-04-02 10:21:25 +0200

    marge into dvelop

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -14,11 +14,10 @@ object main {
 
     SparkConfig.init(Paths.sparkConfigPath)
 
-    val contex = SparkConfig.instance().setSparkConfiguration()
+    val contex = SparkConfig.instance()
 
     val config = SparkConfig.instance()
 
-    val sparkContext = new SparkContext(contex)
 
     /*  val filename = new FileDownloader().downloadWithRedirect("http://data.githubarchive.org/2018-03-01-0.json.gz")
       val fileExtractor = new FileExtractor
@@ -28,11 +27,9 @@ object main {
 
     val path = "/Users/matteobellinaso/Desktop/lynx_accademy/BigData/GroupBigData/downloadJson/2018-03-01-0-1553853054114.json"
 
-    val dataFrameFromJson = Converter.ConvertJSONToDF(path, contex)
-    val rddFromJson = Converter.ConvertJSONToRDD(path, contex)
 
     val commitRDD = new CommitRddOperation[(String, String, Actor, Boolean, Repo, String, Payload)]
-    val commitOp = new CommitDataFrameOperation(sparkContext)
+
 
 
   }
